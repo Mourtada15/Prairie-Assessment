0d85a7bf5afb524b3e70f9582747e90c1118a210
Hassan Mourtada <hassanmourtada15>
2023-09-29 12:08:39 +0300

    section 3 at 12:08

---
+++ Section-3/whatev.js
@@ -0,0 +1,16 @@
+let user_input = parseInt(prompt("Enter a number"));
+
+for (let i = 2; i <= user_input; i++) {
+    let isPrime = true;
+
+    for (let j = 2; j < i; j++) {
+        if (i % j === 0) {
+            isPrime = false;
+            break;
+        }
+    }
+
+    if (isPrime) {
+        console.log(i);
+    }
+}
